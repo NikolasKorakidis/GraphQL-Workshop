f3a90302b16e3747c9e3b88cc7d9b8e6ba0c63fc
nikolaskorakidis <korakidisn@gmail.com>
2020-12-30 14:12:12 +0100

    Server and Data is Ready

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const expressGraphQL = require("express-graphql");
-const schema = require("./schema.js")
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+const cors = require('cors');
 
 const app = express();
 
-app.use("/graphql", expressGraphQL({
-    schema:schema,
-    graphiql: true
-}))
+// enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
+app.use(cors());
 
-app.listen(4000, ()=> console.log("Server is Running"))
+app.use('/graphql', expressGraphQL({
+    schema:schema,
+    graphiql:true
+}));
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000..');
+});
